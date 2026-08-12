737cc7cdd8ee522efce8f905e1f4a51f73c6da8c
blakes24074-source <blakes.24074@obhs.school.nz>
2026-08-12 12:02:17 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,47 @@
+import random
+
+CONFIG = {
+    "options" : [
+        "h",
+        "t"
+    ]
+}
 
 correct = 0
 attempts = 0
 
 def validate_guess(guess : str):
-    try:
-        return int(guess)
-    except:
-        return False
+    # if guess == "h":
+    #     return 0
+    # elif guess == "t":
+    #     return 1
+    for i in range(len(CONFIG["options"])):
+        if CONFIG["options"][i] == guess:
+            return i
+    return -1
 
-def headerortailer_printcorrect(correct : bool):
-    if correct:
+def headerortailer_printcorrect(succ : bool, attempt, corr):
+    if succ:
         print(f"Correct guess!", end="")
     else:
         print(f"Incorrect guess!", end="")
-    print(f" Correct: {correct}, Incorrect: {attempts - correct}, Win rate: {round(correct/attempts * 10000) / 10}%")
+    print(f" Correct: {corr}, Incorrect: {attempt - corr}, Win rate: {round(corr/attempt * 10000) / 100}%")
 
-def headerortailer_check(guess : int, answer : int):
+def headerortailer_check(guess : int, answer : int, attempt, corr):
     if guess == answer:
-        correct += 1
-    headerortailer_printcorrect(guess == answer)
+        corr += 1
+    headerortailer_printcorrect(guess == answer, attempt, corr)
+    return corr
 
-def headerortailer_loop():
+def headerortailer_loop(attempt, corr):
     while True:
-        guess = validate_guess(input("Guess: "))
-        if not guess:
+        guess = validate_guess(input("Guess (h or t): "))
+        ans = random.randint(0, len(CONFIG["options"]) - 1)
+        if guess == -1:
+            print("Invalid guess!")
             continue
-        headerortailer_check()
+        attempt += 1
+        corr = headerortailer_check(guess, ans, attempt, corr)
+
+headerortailer_loop(attempts, correct)
+
