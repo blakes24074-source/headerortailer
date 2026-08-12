933f4ae1ebd4f575a447bda48a27bd6874f8c1cc
blakes24074-source <blakes.24074@obhs.school.nz>
2026-08-13 10:23:51 +1200

    created graphical.py, added docs, fixed "if" functions not having brackets, added intro and namelen config

--- main.py
+++ main.py
@@ -4,44 +4,63 @@ CONFIG = {
     "options" : [
         "h",
         "t"
-    ]
+    ],
+    "namelen" : {
+        "min": 2,
+        "max": 12
+    }
 }
 
-correct = 0
-attempts = 0
 
+# make sure the name is correct, check len and make sure it is made only of letters and not numbers or special characters
+def validate_str(name : str):
+    if (len(name) < CONFIG["namelen"]["min"] or len(name) > CONFIG["namelen"]["max"]):
+        return False
+    return name.isalpha()
+
+# introduction sequence
+def intro():
+    while (True):
+        name = input("Name: ")
+        if (validate_str(name)):
+            print("Welcome, "+name+"!")
+            return
+    
+
+# make sure that the guess is valid and not "t8340u684695864"
 def validate_guess(guess : str):
-    # if guess == "h":
-    #     return 0
-    # elif guess == "t":
-    #     return 1
-    for i in range(len(CONFIG["options"])):
-        if CONFIG["options"][i] == guess:
-            return i
+    for i in range(len(CONFIG["options"])): # check each choice that you can use
+        if (CONFIG["options"][i] == str.lower(guess)): # see if the choice = input
+            return i # return what choice the user made
     return -1
 
-def headerortailer_printcorrect(succ : bool, attempt, corr):
-    if succ:
+# print whether or not the guess was correct
+def headerortailer_printcorrect(succ : bool, attempts, correct):
+    if (succ): # was the guess right? we do seperate prints here to avoid repetition
         print(f"Correct guess!", end="")
     else:
         print(f"Incorrect guess!", end="")
-    print(f" Correct: {corr}, Incorrect: {attempt - corr}, Win rate: {round(corr/attempt * 10000) / 100}%")
+    print(f" Correct: {correct}, Incorrect: {attempts - correct}, Win rate: {round(correct/attempts * 10000) / 100}%") # make the percentage to 2 d.p
 
-def headerortailer_check(guess : int, answer : int, attempt, corr):
-    if guess == answer:
-        corr += 1
-    headerortailer_printcorrect(guess == answer, attempt, corr)
-    return corr
+# check if the answer is correct
+def headerortailer_check(guess : int, answer : int, attempts, correct):
+    if (guess == answer):
+        correct += 1
+    headerortailer_printcorrect(guess == answer, attempts, correct)
+    return correct
 
-def headerortailer_loop(attempt, corr):
-    while True:
+# main loop function
+def headerortailer_loop():
+    
+    while (True):
         guess = validate_guess(input("Guess (h or t): "))
         ans = random.randint(0, len(CONFIG["options"]) - 1)
-        if guess == -1:
+        if (guess == -1):
             print("Invalid guess!")
             continue
-        attempt += 1
-        corr = headerortailer_check(guess, ans, attempt, corr)
+        attempts += 1
+        correct = headerortailer_check(guess, ans, attempts, correct)   # we supply "correct" and "attempts" because of value scope, with "attempts" being for printing
+                                                                        # and "correct" so we can update it later
 
-headerortailer_loop(attempts, correct)
-
+intro()
+headerortailer_loop()
